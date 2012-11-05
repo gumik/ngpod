a7b803e5612c431fd0facb9336cc275f2c025a05
Krzysztof Mataj <gumik0@gmail.com>
2012-11-05 23:20:01 +0100

    Settings to cpp

--- ngpod/ngpod-settings.cpp
+++ ngpod/ngpod-settings.cpp
@@ -1,170 +1,88 @@
 #include <gio/gio.h>
+#include <glibmm.h>
 #include "ngpod-settings.h"
 #include "utils.h"
 
-#define GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), NGPOD_TYPE_SETTINGS, NgpodSettingsPrivate))
+using namespace Glib;
 
-struct _NgpodSettingsPrivate
+namespace ngpod
 {
-    GKeyFile *key_file;
-};
 
-G_DEFINE_TYPE (NgpodSettings, ngpod_settings, G_TYPE_OBJECT);
-
-static const gchar *SETTINGS_PATH;
-static const gchar *SETTINGS_DIR;
-
-static gboolean create_config_dir ();
-static void save_settings (NgpodSettings *self);
-
-static void
-ngpod_settings_dispose (GObject *gobject)
+Settings::~Settings()
 {
-    NgpodSettings *self = NGPOD_SETTINGS (gobject);
+    g_key_file_free (key_file);
+}
 
-    /*
-    * In dispose, you are supposed to free all types referenced from this
-    * object which might themselves hold a reference to self. Generally,
-    * the most simple solution is to unref all members on which you own a
-    * reference.
-    */
+Settings::Settings()
+{
+    SETTINGS_DIR = build_filename (g_get_user_config_dir (), "gumik");
+    SETTINGS_PATH = build_filename (SETTINGS_DIR, "ngpod-gtk.ini");
 
-    /* dispose might be called multiple times, so we must guard against
-    * calling g_object_unref() on an invalid GObject.
-    */
-    /*if (self->priv->an_object)
+    key_file = g_key_file_new ();
+}
+
+bool Settings::Initialize()
+{
+    if (!CreateConfigDir())
     {
-        g_object_unref (self->priv->an_object);
-        self->priv->an_object = NULL;
-    }*/
-
-    /* Chain up to the parent class */
-    G_OBJECT_CLASS (ngpod_settings_parent_class)->dispose (gobject);
-}
-
-static void
-ngpod_settings_finalize (GObject *gobject)
-{
-    NgpodSettings *self = NGPOD_SETTINGS (gobject);
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-
-    g_key_file_free (priv->key_file);
-
-    /* Chain up to the parent class */
-    G_OBJECT_CLASS (ngpod_settings_parent_class)->finalize (gobject);
-}
-
-static void
-ngpod_settings_class_init (NgpodSettingsClass *klass)
-{
-    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-
-    gobject_class->dispose = ngpod_settings_dispose;
-    gobject_class->finalize = ngpod_settings_finalize;
-
-    g_type_class_add_private (klass, sizeof (NgpodSettingsPrivate));
-
-    SETTINGS_DIR = g_build_filename (g_get_user_config_dir (), "gumik", NULL);
-    SETTINGS_PATH = g_build_filename (SETTINGS_DIR, "ngpod-gtk.ini", NULL);
-}
-
-static void
-ngpod_settings_init (NgpodSettings *self)
-{
-    NgpodSettingsPrivate *priv;
-    self->priv = priv = GET_PRIVATE (self);
-
-    priv->key_file = g_key_file_new ();
-}
-
-NgpodSettings *
-ngpod_settings_new (void)
-{
-    GObject *object = g_object_new (NGPOD_TYPE_SETTINGS, NULL);
-    return NGPOD_SETTINGS (object);
-}
-
-gboolean
-ngpod_settings_initialize (NgpodSettings *self)
-{
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-
-    if (!create_config_dir ())
-    {
-        return FALSE;
+        return false;
     }
 
     g_key_file_load_from_file (
-        priv->key_file,
-        SETTINGS_PATH,
+        key_file,
+        SETTINGS_PATH.c_str(),
         G_KEY_FILE_NONE, NULL);
 
-    return TRUE;
+    return true;
 }
 
-void
-ngpod_settings_set_last_date (NgpodSettings *self, const GDate *date)
+void Settings::SetLastDate (const Date& date)
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-
-    gint list[] = { g_date_get_year (date), g_date_get_month (date), g_date_get_day (date) };
-    g_key_file_set_integer_list (priv->key_file, "main", "last_date", list, 3);
-    save_settings (self);
+    gint list[] = { date.get_year(), date.get_month(), date.get_day() };
+    g_key_file_set_integer_list (key_file, "main", "last_date", list, 3);
+    SaveSettings ();
 }
 
-GDate *
-ngpod_settings_get_last_date (NgpodSettings *self)
+Date Settings::GetLastDate() const
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-
     gsize length = 0;
-    gint *list = g_key_file_get_integer_list (priv->key_file, "main", "last_date", &length, NULL);
+    gint *list = g_key_file_get_integer_list (key_file, "main", "last_date", &length, NULL);
 
-    GDate *date = NULL;
+    Date date;
 
     if (list == NULL || length < 3)
     {
-        date = g_date_new_dmy (1, 1, 1);
+        date = Date(Date::MONDAY, Date::JANUARY, 1);
     }
     else
     {
-        date = g_date_new_dmy (list[2], list[1], list[0]);
+        date = Date (static_cast<Date::Day>(list[2]), static_cast<Date::Month>(list[1]), list[0]);
         g_free (list);
     }
 
     return date;
 }
 
-void
-ngpod_settings_set_dir (NgpodSettings *self, const gchar *dir)
+void Settings::SetDir (const ustring& dir)
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-
-    g_key_file_set_string (priv->key_file, "main", "dir", dir);
-    save_settings (self);
+    g_key_file_set_string (key_file, "main", "dir", dir.c_str());
+    SaveSettings();
 }
 
-gchar *
-ngpod_settings_get_dir (const NgpodSettings *self)
+ustring Settings::GetDir() const
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-    return g_key_file_get_string (priv->key_file, "main", "dir", NULL);
+    return g_key_file_get_string (key_file, "main", "dir", NULL);
 }
 
-gchar *
-ngpod_settings_get_log_file (NgpodSettings *self)
+ustring Settings::GetLogFile() const
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-    return g_key_file_get_string (priv->key_file, "main", "log_file", NULL);
+    return g_key_file_get_string (key_file, "main", "log_file", NULL);
 }
 
-GTimeSpan
-ngpod_settings_get_time_span (NgpodSettings *self)
+GTimeSpan Settings::GetTimeSpan() const
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
-
     gsize length = 0;
-    gint *list = g_key_file_get_integer_list (priv->key_file, "main", "time_span", &length, NULL);
+    gint *list = g_key_file_get_integer_list (key_file, "main", "time_span", &length, NULL);
 
     GTimeSpan time_span = 0;
 
@@ -181,13 +99,10 @@ ngpod_settings_get_time_span (NgpodSettings *self)
     return time_span;
 }
 
-/* private functions */
-
-static gboolean
-create_config_dir ()
+bool Settings::CreateConfigDir()
 {
     GError *error = NULL;
-    GFile *dir = g_file_new_for_path (SETTINGS_DIR);
+    GFile *dir = g_file_new_for_path (SETTINGS_DIR.c_str());
     g_file_make_directory_with_parents (dir, NULL, &error);
 
     gboolean status = TRUE;
@@ -208,12 +123,10 @@ create_config_dir ()
     return status;
 }
 
-static void
-save_settings (NgpodSettings *self)
+void Settings::SaveSettings()
 {
-    NgpodSettingsPrivate *priv = GET_PRIVATE (self);
     GError *error = NULL;
-    GFile *file = g_file_new_for_path (SETTINGS_PATH);
+    GFile *file = g_file_new_for_path (SETTINGS_PATH.c_str());
     GFileOutputStream *os = g_file_replace (file, NULL, FALSE, G_FILE_CREATE_NONE, NULL, &error);
 
     if (error != NULL)
@@ -224,7 +137,7 @@ save_settings (NgpodSettings *self)
     else
     {
         gsize length = 0;
-        gchar *data = g_key_file_to_data (priv->key_file, &length, NULL);
+        gchar *data = g_key_file_to_data (key_file, &length, NULL);
 
         error = NULL;
         gsize bytes_written = 0;
@@ -247,4 +160,6 @@ save_settings (NgpodSettings *self)
 
     g_clear_object (&os);
     g_object_unref (file);
+}
+
 }
--- ngpod/ngpod-settings.h
+++ ngpod/ngpod-settings.h
@@ -2,47 +2,38 @@
 #define __NGPOD_SETTINGS_H__
 
 #include <glib-object.h>
+#include <glibmm/date.h>
 
-#define NGPOD_TYPE_SETTINGS                  (ngpod_settings_get_type ())
-#define NGPOD_SETTINGS(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), NGPOD_TYPE_SETTINGS, NgpodSettings))
-#define NGPOD_IS_SETTINGS(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NGPOD_TYPE_SETTINGS))
-#define NGPOD_SETTINGS_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), NGPOD_TYPE_SETTINGS, NgpodSettingsClass))
-#define NGPOD_IS_SETTINGS_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), NGPOD_TYPE_SETTINGS))
-#define NGPOD_SETTINGS_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), NGPOD_TYPE_SETTINGS, NgpodSettingsClass))
-
-typedef struct _NgpodSettings        NgpodSettings;
-typedef struct _NgpodSettingsClass   NgpodSettingsClass;
-typedef struct _NgpodSettingsPrivate NgpodSettingsPrivate;
-
-struct _NgpodSettings
+namespace ngpod
 {
-    GObject          parent_instance;
-    NgpodSettingsPrivate *priv;
+
+class Settings
+{
+public:
+    Settings();
+    virtual ~Settings();
+
+    bool Initialize();
+
+    void SetLastDate(const Glib::Date& date);
+    Glib::Date GetLastDate() const;
+
+    void SetDir(const Glib::ustring& dir);
+    Glib::ustring GetDir() const;
+
+    Glib::ustring GetLogFile() const;
+
+    GTimeSpan GetTimeSpan() const;
+
+private:
+    bool CreateConfigDir();
+    void SaveSettings();
+
+    GKeyFile *key_file;
+    Glib::ustring SETTINGS_PATH;
+    Glib::ustring SETTINGS_DIR;
 };
 
-struct _NgpodSettingsClass
-{
-    GObjectClass parent_class;
-};
-
-/* used by NGPOD_TYPE_SETTINGS */
-GType ngpod_settings_get_type (void);
-
-NgpodSettings *ngpod_settings_new (void);
-
-/*
- * Method definitions.
- */
-gboolean ngpod_settings_initialize (NgpodSettings *self);
-
-void ngpod_settings_set_last_date (NgpodSettings *self, const GDate *date);
-GDate *ngpod_settings_get_last_date (NgpodSettings *self);
-
-void ngpod_settings_set_dir (NgpodSettings *self, const gchar *dir);
-gchar *ngpod_settings_get_dir (const NgpodSettings *self);
-
-gchar *ngpod_settings_get_log_file (NgpodSettings *self);
-
-GTimeSpan ngpod_settings_get_time_span (NgpodSettings *self);
+}
 
 #endif /* __NGPOD_SETTINGS_H__ */
