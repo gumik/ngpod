eedbf517f570479df99748527ccad264c71c21e6
Krzysztof Mataj <gumik0@gmail.com>
2012-11-06 20:03:25 +0100

    Wallpaper to cpp

--- ngpod/ngpod-wallpaper.cpp
+++ ngpod/ngpod-wallpaper.cpp
@@ -2,119 +2,44 @@
 #include <mateconf/mateconf.h>
 #include "ngpod-wallpaper.h"
 
-#define GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), NGPOD_TYPE_WALLPAPER, NgpodWallpaperPrivate))
+using namespace Glib;
 
-struct _NgpodWallpaperPrivate
+namespace ngpod
 {
-    gchar *dir;
-};
 
-G_DEFINE_TYPE (NgpodWallpaper, ngpod_wallpaper, G_TYPE_OBJECT);
-
-static gchar *get_path_from_date (GDateTime *now, const gchar *dir);
-
-static void
-ngpod_wallpaper_dispose (GObject *gobject)
+Wallpaper::Wallpaper(const ustring& dir)
+    : dir(dir)
 {
-    NgpodWallpaper *self = NGPOD_WALLPAPER (gobject);
-
-    /*
-    * In dispose, you are supposed to free all types referenced from this
-    * object which might themselves hold a reference to self. Generally,
-    * the most simple solution is to unref all members on which you own a
-    * reference.
-    */
-
-    /* dispose might be called multiple times, so we must guard against
-    * calling g_object_unref() on an invalid GObject.
-    */
-    /*if (self->priv->an_object)
-    {
-        g_object_unref (self->priv->an_object);
-        self->priv->an_object = NULL;
-    }*/
-
-    /* Chain up to the parent class */
-    G_OBJECT_CLASS (ngpod_wallpaper_parent_class)->dispose (gobject);
 }
 
-static void
-ngpod_wallpaper_finalize (GObject *gobject)
+bool Wallpaper::SetFromData(const char *data, gsize data_length, GError **error)
 {
-    NgpodWallpaper *self = NGPOD_WALLPAPER (gobject);
-    NgpodWallpaperPrivate *priv = GET_PRIVATE (self);
-
-    g_free (priv->dir);
-
-    /* Chain up to the parent class */
-    G_OBJECT_CLASS (ngpod_wallpaper_parent_class)->finalize (gobject);
-}
-
-static void
-ngpod_wallpaper_class_init (NgpodWallpaperClass *klass)
-{
-    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-
-    gobject_class->dispose = ngpod_wallpaper_dispose;
-    gobject_class->finalize = ngpod_wallpaper_finalize;
-
-    g_type_class_add_private (klass, sizeof (NgpodWallpaperPrivate));
-}
-
-static void
-ngpod_wallpaper_init (NgpodWallpaper *self)
-{
-    NgpodWallpaperPrivate *priv;
-    self->priv = priv = GET_PRIVATE (self);
-}
-
-NgpodWallpaper *
-ngpod_wallpaper_new (gchar *dir)
-{
-    GObject *object = g_object_new (NGPOD_TYPE_WALLPAPER, NULL);
-
-    NgpodWallpaperPrivate *priv = GET_PRIVATE (object);
-    priv->dir = dir;
-
-    return NGPOD_WALLPAPER (object);
-}
-
-gboolean
-ngpod_wallpaper_set_from_data (NgpodWallpaper *self, const char *data, gsize data_length, GError **error)
-{
-    NgpodWallpaperPrivate *priv = GET_PRIVATE (self);
-
     // Prepare path
-    GDateTime *now = g_date_time_new_now_local ();
-    gchar *path = get_path_from_date (now, priv->dir);
-    g_date_time_unref (now);
+    DateTime now = DateTime::create_now_local();
+    ustring path = GetPathFromDate (now, dir);
 
     // Write data to file
-    GFile *file = g_file_new_for_path (path);
-    //g_free (path);
+    GFile *file = g_file_new_for_path (path.c_str());
 
     GFileOutputStream *stream = g_file_create (file, G_FILE_CREATE_NONE, NULL, error);
 
     if (stream == NULL)
     {
-        g_free (path);
         g_object_unref (file);
-        return FALSE;
+        return false;
     }
 
     gsize bytes_written = 0;
     if (!g_output_stream_write_all (G_OUTPUT_STREAM (stream), data, data_length, &bytes_written, NULL, error))
     {
-        g_free (path);
         g_object_unref (stream);
-        return FALSE;
+        return false;
     }
 
     if (!g_output_stream_close (G_OUTPUT_STREAM (stream), NULL, error))
     {
-        g_free (path);
         g_object_unref (stream);
-        return FALSE;
+        return false;
     }
 
     g_object_unref (stream);
@@ -122,25 +47,22 @@ ngpod_wallpaper_set_from_data (NgpodWallpaper *self, const char *data, gsize dat
     // Change desktop background
     MateConfEngine *conf = mateconf_engine_get_default ();
     gchar *value = mateconf_engine_get_string (conf, "/desktop/mate/background/picture_filename", NULL);
-    if (!mateconf_engine_set_string (conf, "/desktop/mate/background/picture_filename", path, error))
+    if (!mateconf_engine_set_string (conf, "/desktop/mate/background/picture_filename", path.c_str(), error))
     {
-        g_free (path);
         mateconf_engine_unref (conf);
-        return FALSE;
+        return false;
     }
 
-    g_free (path);
     mateconf_engine_unref (conf);
 
-    return TRUE;
+    return true;
 }
 
 /* private methods */
-static gchar *
-get_path_from_date (GDateTime *now, const gchar *dir)
+ustring GetPathFromDate(const DateTime& now, const ustring& dir)
 {
-    gchar *filename = g_date_time_format (now, "pod-%F.png");
-    gchar *path = g_build_filename (dir, filename, NULL);
-    g_free (filename);
-    return path;
+    ustring filename = now.format("pod-%F.png");
+    return build_filename(dir, filename);
+}
+
 }
--- ngpod/ngpod-wallpaper.h
+++ ngpod/ngpod-wallpaper.h
@@ -1,38 +1,25 @@
 #ifndef __NGPOD_WALLPAPER_H__
 #define __NGPOD_WALLPAPER_H__
 
-#include <glib-object.h>
+#include <glib.h>
+#include <glibmm.h>
+#include <glibmm/datetime.h>
 
-#define NGPOD_TYPE_WALLPAPER                  (ngpod_wallpaper_get_type ())
-#define NGPOD_WALLPAPER(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), NGPOD_TYPE_WALLPAPER, NgpodWallpaper))
-#define NGPOD_IS_WALLPAPER(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NGPOD_TYPE_WALLPAPER))
-#define NGPOD_WALLPAPER_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), NGPOD_TYPE_WALLPAPER, NgpodWallpaperClass))
-#define NGPOD_IS_WALLPAPER_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), NGPOD_TYPE_WALLPAPER))
-#define NGPOD_WALLPAPER_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), NGPOD_TYPE_WALLPAPER, NgpodWallpaperClass))
-
-typedef struct _NgpodWallpaper        NgpodWallpaper;
-typedef struct _NgpodWallpaperClass   NgpodWallpaperClass;
-typedef struct _NgpodWallpaperPrivate NgpodWallpaperPrivate;
-
-struct _NgpodWallpaper
+namespace ngpod
 {
-    GObject          parent_instance;
-    NgpodWallpaperPrivate *priv;
+
+class Wallpaper
+{
+public:
+    Wallpaper(const Glib::ustring& dir);
+    bool SetFromData(const char* data, gsize data_length, GError** error);
+
+private:
+    Glib::ustring GetPathFromDate(const Glib::DateTime& now, const Glib::ustring& dir);
+
+    Glib::ustring dir;
 };
 
-struct _NgpodWallpaperClass
-{
-    GObjectClass parent_class;
-};
-
-/* used by NGPOD_TYPE_WALLPAPER */
-GType ngpod_wallpaper_get_type (void);
-
-NgpodWallpaper *ngpod_wallpaper_new (gchar *dir);
-
-/*
- * Method definitions.
- */
-gboolean ngpod_wallpaper_set_from_data (NgpodWallpaper *self, const char *data, gsize data_length, GError **error);
+}
 
 #endif /* __NGPOD_WALLPAPER_H__ */
