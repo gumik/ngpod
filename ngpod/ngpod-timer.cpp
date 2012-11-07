2fefceddae520e3490a5722597727586c32a3800
Krzysztof Mataj <gumik0@gmail.com>
2012-11-07 22:11:12 +0100

    Timer to cpp

--- ngpod/ngpod-timer.cpp
+++ ngpod/ngpod-timer.cpp
@@ -1,178 +1,75 @@
 #include "ngpod-timer.h"
 #include "utils.h"
 
-#define GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), NGPOD_TYPE_TIMER, NgpodTimerPrivate))
+using namespace Glib;
 
-struct _NgpodTimerPrivate
+namespace ngpod
 {
-    NgpodWatcher *watcher;
-    NgpodSettings *settings;
-    NgpodPresenter *presenter;
-    NgpodWallpaper *wallpaper;
-};
 
-G_DEFINE_TYPE (NgpodTimer, ngpod_timer, G_TYPE_OBJECT);
-
-static const int NOT_NEEDED_TIMEOUT = 60;
-static const int FAILED_TIMEOUT = 60;
-static const int SUCCESS_TIMEOUT = 5 * 60;
-static const int SUCCESS_NO_IMAGE_TIMEOUT = 15 * 60;
-
-static gboolean ngpod_timer_tick (gpointer data);
-static void watcher_finished_event (NgpodWatcher *watche, gpointer data);
-static void add_timeout (NgpodTimer *self, gint seconds);
-static void update_last_date_in_settings (NgpodTimer *self);
-static void presenter_made_choice_event (NgpodPresenter *presenter, gpointer data);
-
-static void
-ngpod_timer_dispose (GObject *gobject)
+Timer::Timer(Watcher& watcher, Settings& settings, Presenter& presenter,
+    Wallpaper& wallpaper)
+    : watcher(watcher)
+    , wallpaper(wallpaper)
+    , settings(settings)
+    , presenter(presenter)
 {
-    NgpodTimer *self = NGPOD_TIMER (gobject);
+    watcher.signal_UpdateFinished().connect(sigc::mem_fun(*this, &Timer::WatcherFinishedCallback));
+    presenter.signal_MadeChoice().connect(sigc::mem_fun(*this, &Timer::PresenterMadeChoiceCallback));
+}
 
-    /*
-    * In dispose, you are supposed to free all types referenced from this
-    * object which might themselves hold a reference to self. Generally,
-    * the most simple solution is to unref all members on which you own a
-    * reference.
-    */
+void Timer::Start()
+{
+    AddTimeout (1);
+}
 
-    /* dispose might be called multiple times, so we must guard against
-    * calling g_object_unref() on an invalid GObject.
-    */
-    /*if (self->priv->an_object)
+bool Timer::Tick ()
+{
+    DateTime now = DateTime::create_now_local();
+    watcher.Tick(now);
+
+    return false;
+}
+
+void Timer::WatcherFinishedCallback()
+{
+    Watcher::Status status = watcher.GetStatus();
+
+    if (status == Watcher::NOT_NEEDED)
     {
-        g_object_unref (self->priv->an_object);
-        self->priv->an_object = NULL;
-    }*/
-
-  /* Chain up to the parent class */
-  G_OBJECT_CLASS (ngpod_timer_parent_class)->dispose (gobject);
-}
-
-static void
-ngpod_timer_finalize (GObject *gobject)
-{
-    NgpodTimer *self = NGPOD_TIMER (gobject);
-    NgpodTimerPrivate *priv = GET_PRIVATE (self);
-
-    g_clear_object (&priv->watcher);
-    g_clear_object (&priv->settings);
-    g_clear_object (&priv->presenter);
-    g_clear_object (&priv->wallpaper);
-
-    /* Chain up to the parent class */
-    G_OBJECT_CLASS (ngpod_timer_parent_class)->finalize (gobject);
-}
-
-static void
-ngpod_timer_class_init (NgpodTimerClass *klass)
-{
-    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-
-    gobject_class->dispose = ngpod_timer_dispose;
-    gobject_class->finalize = ngpod_timer_finalize;
-
-    g_type_class_add_private (klass, sizeof (NgpodTimerPrivate));
-}
-
-static void
-ngpod_timer_init (NgpodTimer *self)
-{
-    NgpodTimerPrivate *priv;
-    self->priv = priv = GET_PRIVATE (self);
-}
-
-NgpodTimer *
-ngpod_timer_new (
-    NgpodWatcher *watcher,
-    NgpodSettings *settings,
-    NgpodPresenter *presenter,
-    NgpodWallpaper *wallpaper)
-{
-    GObject *object = g_object_new (NGPOD_TYPE_TIMER, NULL);
-    NgpodTimerPrivate *priv = GET_PRIVATE (object);
-
-    priv->watcher = watcher;
-    g_object_ref (watcher);
-    g_signal_connect (watcher, "update-finished", G_CALLBACK (watcher_finished_event), object);
-
-    priv->settings = settings;
-    g_object_ref (settings);
-
-    priv->presenter = presenter;
-    g_object_ref (presenter);
-    g_signal_connect (presenter, "made-choice", G_CALLBACK (presenter_made_choice_event), object);
-
-    priv->wallpaper = wallpaper;
-    g_object_ref (wallpaper);
-
-    return NGPOD_TIMER (object);
-}
-
-void
-ngpod_timer_start (NgpodTimer *self)
-{
-    add_timeout (self, 1);
-}
-
-/* private functions */
-static gboolean
-ngpod_timer_tick (gpointer data)
-{
-    NgpodTimer *self = (NgpodTimer *) data;
-    NgpodTimerPrivate *priv = GET_PRIVATE (self);
-
-    GDateTime *now = g_date_time_new_now_local();
-    ngpod_watcher_tick (priv->watcher, now);
-    g_date_time_unref (now);
-
-    return FALSE;
-}
-
-static void
-watcher_finished_event (NgpodWatcher *watcher, gpointer data)
-{
-    NgpodTimer *self = (NgpodTimer *) data;
-    NgpodTimerPrivate *priv = GET_PRIVATE (self);
-    NgpodWatcherStatus status = ngpod_watcher_get_status (watcher);
-
-    if (status == NGPOD_WATCHER_STATUS_NOT_NEEDED)
-    {
-        add_timeout (self, NOT_NEEDED_TIMEOUT);
+        AddTimeout (NOT_NEEDED_TIMEOUT);
         log_message ("Timer", "NOT_NEEDED");
     }
-    else if (status == NGPOD_WATCHER_STATUS_UPDATED)
+    else if (status == Watcher::UPDATED)
     {
-        const NgpodDownloader *downloader = ngpod_watcher_get_downloader (watcher);
-        NgpodDownloaderStatus downloader_status = ngpod_downloader_get_status (downloader);
+        const Downloader& downloader = watcher.GetDownloader();
+        Downloader::Status downloader_status = downloader.GetStatus();
 
         switch (downloader_status)
         {
-            case NGPOD_DOWNLOADER_STATUS_FAILED:
+            case Downloader::FAILED:
                 log_message ("Timer", "DOWNLOADER_FAILED");
-                add_timeout (self, FAILED_TIMEOUT);
+                AddTimeout(FAILED_TIMEOUT);
                 break;
 
-            case NGPOD_DOWNLOADER_STATUS_FAILED_GET_IMAGE:
+            case Downloader::FAILED_GET_IMAGE:
                 log_message ("Timer", "DOWNLOADER_FAILED_GET_IMAGE");
-                add_timeout (self, FAILED_TIMEOUT);
+                AddTimeout(FAILED_TIMEOUT);
                 break;
 
-            case NGPOD_DOWNLOADER_STATUS_SUCCESS_NO_IMAGE:
+            case Downloader::SUCCESS_NO_IMAGE:
                 log_message ("Timer", "DOWNLOADER_SUCCESS_NO_IMAGE");
-                update_last_date_in_settings (self);
-                add_timeout (self, SUCCESS_NO_IMAGE_TIMEOUT);
+                UpdateLastDateInSettings();
+                AddTimeout(SUCCESS_NO_IMAGE_TIMEOUT);
                 break;
 
-            case NGPOD_DOWNLOADER_STATUS_SUCCESS:
+            case Downloader::SUCCESS:
                 log_message ("Timer", "DOWNLOADER_SUCCESS");
 
-                ngpod_presenter_notify (
-                    priv->presenter,
-                    ngpod_downloader_get_data (downloader),
-                    ngpod_downloader_get_data_length (downloader),
-                    ngpod_downloader_get_title (downloader),
-                    ngpod_downloader_get_description (downloader));
+                presenter.Notify(
+                    downloader.GetData(),
+                    downloader.GetDataLength(),
+                    downloader.GetTitle(),
+                    downloader.GetDescription());
                 break;
         }
     }
@@ -182,47 +79,41 @@ watcher_finished_event (NgpodWatcher *watcher, gpointer data)
     }
 }
 
-static void
-add_timeout (NgpodTimer *self, gint seconds)
+void Timer::AddTimeout (int seconds)
 {
-    g_timeout_add_seconds (seconds, ngpod_timer_tick, (gpointer) self);
+    signal_timeout().connect_seconds(sigc::mem_fun(*this, &Timer::Tick), seconds);
 }
 
-static void
-update_last_date_in_settings (NgpodTimer *self)
+void Timer::UpdateLastDateInSettings ()
 {
-    NgpodTimerPrivate *priv = GET_PRIVATE (self);
-
-    const GDate *last_date = ngpod_watcher_get_last_date (priv->watcher);
-    ngpod_settings_set_last_date (priv->settings, last_date);
+    Date last_date = watcher.GetLastDate();
+    settings.SetLastDate(last_date);
 }
 
-static void
-presenter_made_choice_event (NgpodPresenter *presenter, gpointer data)
+void Timer::PresenterMadeChoiceCallback()
 {
-    NgpodTimer *self = (NgpodTimer *) data;
-    NgpodTimerPrivate *priv = GET_PRIVATE (self);
-
-    if (ngpod_presenter_is_accepted (presenter))
+    if (presenter.IsAccepted())
     {
         GError *error = NULL;
 
-        const NgpodDownloader *downloader = ngpod_watcher_get_downloader (priv->watcher);
-        const char * data = ngpod_downloader_get_data (downloader);
-        gsize data_length = ngpod_downloader_get_data_length (downloader);
+        const Downloader& downloader = watcher.GetDownloader();
+        const char * data = downloader.GetData();
+        int data_length = downloader.GetDataLength();
 
-        if (!ngpod_wallpaper_set_from_data(priv->wallpaper, data, data_length, &error))
+        if (!wallpaper.SetFromData(data, data_length, &error))
         {
             GString *msg = g_string_new ("");
             g_string_printf (msg, "Error while setting wallpaper:\n%s", error->message);
-            ngpod_presenter_show_error (priv->presenter, msg->str);
+            presenter.ShowError (msg->str);
             log_message ("Timer", msg->str);
             g_string_free (msg, TRUE);
             g_error_free (error);
         }
     }
 
-    ngpod_presenter_hide (presenter);
-    update_last_date_in_settings (self);
-    add_timeout (self, SUCCESS_TIMEOUT);
+    presenter.Hide();
+    UpdateLastDateInSettings();
+    AddTimeout(SUCCESS_TIMEOUT);
+}
+
 }
--- ngpod/ngpod-timer.h
+++ ngpod/ngpod-timer.h
@@ -1,46 +1,40 @@
 #ifndef __NGPOD_TIMER_H__
 #define __NGPOD_TIMER_H__
 
-#include <glib-object.h>
+#include <glibmm.h>
 #include "ngpod-watcher.h"
 #include "ngpod-settings.h"
 #include "ngpod-presenter.h"
 #include "ngpod-wallpaper.h"
 
-#define NGPOD_TYPE_TIMER                  (ngpod_timer_get_type ())
-#define NGPOD_TIMER(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), NGPOD_TYPE_TIMER, NgpodTimer))
-#define NGPOD_IS_TIMER(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NGPOD_TYPE_TIMER))
-#define NGPOD_TIMER_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), NGPOD_TYPE_TIMER, NgpodTimerClass))
-#define NGPOD_IS_TIMER_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), NGPOD_TYPE_TIMER))
-#define NGPOD_TIMER_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), NGPOD_TYPE_TIMER, NgpodTimerClass))
-
-typedef struct _NgpodTimer        NgpodTimer;
-typedef struct _NgpodTimerClass   NgpodTimerClass;
-typedef struct _NgpodTimerPrivate NgpodTimerPrivate;
-
-struct _NgpodTimer
+namespace ngpod
 {
-    GObject          parent_instance;
-    NgpodTimerPrivate *priv;
+
+class Timer
+{
+public:
+    Timer(Watcher& watcher, Settings& settings, Presenter& presenter,
+        Wallpaper& wallpaper);
+    void Start();
+
+private:
+    bool Tick ();
+    void WatcherFinishedCallback ();
+    void AddTimeout (int seconds);
+    void UpdateLastDateInSettings ();
+    void PresenterMadeChoiceCallback ();
+
+    Watcher& watcher;
+    Settings& settings;
+    Presenter& presenter;
+    Wallpaper& wallpaper;
+
+    static const int NOT_NEEDED_TIMEOUT = 60;
+    static const int FAILED_TIMEOUT = 60;
+    static const int SUCCESS_TIMEOUT = 5 * 60;
+    static const int SUCCESS_NO_IMAGE_TIMEOUT = 15 * 60;
 };
 
-struct _NgpodTimerClass
-{
-    GObjectClass parent_class;
-};
-
-/* used by NGPOD_TYPE_TIMER */
-GType ngpod_timer_get_type (void);
-
-NgpodTimer *ngpod_timer_new (
-	NgpodWatcher *watcher,
-	NgpodSettings *settings,
-	NgpodPresenter *presenter,
-	NgpodWallpaper *wallpaper);
-
-/*
- * Method definitions.
- */
-void ngpod_timer_start (NgpodTimer *self);
+}
 
 #endif /* __NGPOD_TIMER_H__ */
